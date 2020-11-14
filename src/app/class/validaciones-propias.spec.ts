6caec45c7d0136b86d2b0072d3b7dd4c6940fd15
echaparro12 <aaaanonimo143@gmail.com>
2020-11-14 09:36:59 -0500

    Auto-generated commit

--- src/app/class/validaciones-propias.spec.ts
+++
@@ -1,7 +0,0 @@
-import { validacionesPropias } from './validaciones-propias';
-
-describe('CvalidacionesPropias', () => {
-  it('should create an instance', () => {
-    expect(new validacionesPropias()).toBeTruthy();
-  });
-});
--- src/app/models/usuarios.ts
+++
@@ -1,7 +0,0 @@
-export interface usuarios{
-    identificacion:number,
-    nombre:string,
-    apellido:String,
-    correo:string,
-    contraseña:string
-}
